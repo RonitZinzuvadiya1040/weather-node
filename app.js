4e608fb58be74b0ea42ed417527a5849ebf3a0be
Ronit Zinzuvadiya <96730598+RonitZinzuvadiya1040@users.noreply.github.com>
2022-03-14 16:29:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,18 +24,16 @@ document.getElementById("senddata").addEventListener("click", function () {
     forcasting: forcasting,
   };
 
-    
   myFormData.push(formdata);
   // console.log(data);
   // console.log(formdata);
-  
-  //// console.log(myFormData);
+  console.log(myFormData);
   console.log(`Cityname: ${cityname}`)
   console.log(`Date: ${date}`)
-  console.log(`Time: ${time}`)
-  console.log(`Temprature: ${temprature}`)
-  console.log(`Temprature Unit: ${tempratureunit}`)
-  console.log(`ForCasting: ${forcasting}`)
+  console.log(`time: ${time}`)
+  console.log(`temprature: ${temprature}`)
+  console.log(`tempratureunit: ${tempratureunit}`)
+  console.log(`forcasting: ${forcasting}`)
 
 
 
@@ -61,3 +59,50 @@ document.getElementById("senddata").addEventListener("click", function () {
  
 });
 
+// document.getElementById("getdata").addEventListener("click", function () {
+//   const cityname = document.getElementById("cname").value;
+
+//   const date = document.getElementById("date").value;
+
+//   const time = document.getElementById("time").value;
+
+//   const temprature = document.getElementById("temprature").value;
+
+//   const tempratureunit = document.getElementById("tempratureunit").value;
+
+//   const forcasting = document.getElementById("forcasting").value;
+
+//   const formdata = {
+//     city: cityname,
+//     date: date,
+//     time: time,
+//     temprature: temprature,
+//     tempratureunit: tempratureunit,
+//     forcasting: forcasting,
+//   };
+
+//   myFormData.pop(formdata);
+//   // console.log(data);
+//   // console.log(formdata);
+//   console.log(myFormData);
+
+//   let nHTML = "";
+//   myFormData.forEach(function (item) {
+//     nHTML +=
+//       "<li> Cityname: " +
+//       item.city +
+//       "<li> Date: " +
+//       item.date +
+//       "<li> Time: " +
+//       item.time +
+//       "<li> Temprature: " +
+//       item.temprature +
+//       "<li> Temprature Unit: " +
+//       item.tempratureunit +
+//       "<li> Forcasting: " +
+//       item.forcasting;
+//   });
+
+//   document.getElementById("showdata").innerHTML = "<ul>" + nHTML + "</ul>";
+ 
+// });
